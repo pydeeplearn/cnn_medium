6720eac0406f8d282105be4b7c536ab244b29783
minghuadev <minghuadev@users.noreply.github.com>
2022-06-28 18:09:11 -0700

    converted model to v1 frozen graph

--- convert_model_to_v1.py
+++ convert_model_to_v1.py
@@ -24,7 +24,7 @@ if os.path.isfile("trained_model.h5"):
 
 if not loaded_ok:
     print("Could not load \"trained_model.h5\"")
-    if os.path.isdir("trained_model") and os.path.isdir("trained_model/variables"):
+    if os.path.isdir("trained_model"):
         # it needs "saved_model.pb" and "variables/" under "trained_model"
         from tensorflow.keras.models import load_model
         try:
@@ -33,6 +33,7 @@ if not loaded_ok:
             print("Loaded full model from trained_model/")
             loaded_ok = True
         except Exception as e:
+            print("Exception: ", repr(e))
             pass
 
 if not loaded_ok:
@@ -156,4 +157,39 @@ else:
         else:
             prediction = 'cat'
         print("Single prediction: ", idx, " is ", prediction)
+
+    # save to single pb file: ref4
+    # https://stackoverflow.com/questions/58119155/freezing-graph-to-pb-in-tensorflow2
+    import tensorflow as tf
+    from tensorflow import keras
+    from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2
+    import numpy as np
+
+    model = classifier
+
+    full_model = tf.function(lambda x: model(x))
+    full_model = full_model.get_concrete_function(
+        tf.TensorSpec(model.inputs[0].shape, model.inputs[0].dtype))
+
+    # Get frozen ConcreteFunction
+    frozen_func = convert_variables_to_constants_v2(full_model)
+    frozen_func.graph.as_graph_def()
+
+    layers = [op.name for op in frozen_func.graph.get_operations()]
+    print("-" * 50)
+    print("Frozen model layers: ")
+    for layer in layers:
+        print(layer)
+
+    print("-" * 50)
+    print("Frozen model inputs: ")
+    print(frozen_func.inputs)
+    print("Frozen model outputs: ")
+    print(frozen_func.outputs)
+
+    # Save frozen graph from frozen ConcreteFunction to hard drive
+    tf.io.write_graph(graph_or_graph_def=frozen_func.graph,
+                      logdir="./frozen_models",
+                      name="frozen_graph.pb",
+                      as_text=False)
 
